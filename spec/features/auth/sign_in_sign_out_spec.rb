ee19847b7ed4714dbc8fd593c6cf525f2c594612
Jack Gould <Johnhilsmangould@gmail.com>
2025-01-31 14:40:23 -0700

    add init test and spec helpers

---
+++ spec/features/auth/sign_in_sign_out_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+
+RSpec.feature "Sign in and sign out", type: :feature do
+    scenario "User signs in and signs out" do
+        user = User.create!(email: "test@example.com", password: "password")
+
+            visit new_user_session_path
+
+            fill_in "Email", with: user.email
+            fill_in "Password", with: user.password
+            click_button "Log in"
+
+            expect(page).to have_content("Signed in successfully")
+
+            click_button "Logout"
+
+            expect(page).to have_content("Signed out successfully")
+        end
+end
+    
