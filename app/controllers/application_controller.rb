dffab05f0eacae6ac527cc475f05a5368a253682
Jack Gould <Johnhilsmangould@gmail.com>
2025-02-07 14:48:58 -0700

    add applicaiton_controller updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+    def after_sign_in_path_for(resource)
+        if resource.onboarding_complete?
+            #will need to upadte onboarding_complete as
+            #that is not method will not work right now
+            # dashboard_path
+        else
+            onboarding_path(:personal_info) # Start the onboarding process
+        end
+    end
 end
