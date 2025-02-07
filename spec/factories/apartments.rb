58177367f2e7ad5dbc8915705b2b83319b3f02fb
Jack Gould <Johnhilsmangould@gmail.com>
2025-02-07 14:50:27 -0700

    add onboarding flow spec and new factory for apartment

--- spec/factories/apartments.rb
+++ spec/factories/apartments.rb
@@ -6,5 +6,14 @@ FactoryBot.define do
     zip_code { "MyString" }
     country { "MyString" }
     full_address { "MyString" }
+
+    trait :real_address do
+      street_address { "3463 Walnut Street" }
+      city { "Denver" }
+      state { "Colorado" }
+      zip_code { "80205" }
+      country { "United States" }
+      full_address { "Edit at River North, 3463, Walnut Street, Five Points, Denver, Colorado, 80205, United States" }
+    end
   end
 end
