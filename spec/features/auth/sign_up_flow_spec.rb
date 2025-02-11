fd43cf4f860dd80dfc2eb89bba71feef6c12f811
Jack Gould <Johnhilsmangould@gmail.com>
2025-02-11 13:30:39 -0700

    add specs

--- spec/features/auth/sign_up_flow_spec.rb
+++ spec/features/auth/sign_up_flow_spec.rb
@@ -17,7 +17,7 @@ RSpec.feature "User sign up flow", type: :feature do
             fill_in "Unit number", with: " 1363"
             fill_in "City", with: "Denver"
             fill_in "State", with: "CO"
-            fill_in "Zipcode", with: "80205"
+            fill_in "Zip code", with: "80205"
             fill_in "Country", with: "USA"
 
             click_button "Next"
@@ -39,7 +39,7 @@ RSpec.feature "User sign up flow", type: :feature do
             # expect(current_path).to eq(user_dashboard_path)
         end
 
-        xscenario "New user signs up with an invalid address" do
+        scenario "New user signs up with an invalid address" do
             apartment = create(:apartment, :real_address)
             visit new_user_registration_path
 
@@ -54,7 +54,7 @@ RSpec.feature "User sign up flow", type: :feature do
             fill_in "Unit number", with: " 1363"
             fill_in "City", with: "Steamboat Springs"
             fill_in "State", with: "CO"
-            fill_in "Zipcode", with: "80205"
+            fill_in "Zip code", with: "80205"
             fill_in "Country", with: "USA"
 
             click_button "Next"
@@ -69,12 +69,61 @@ RSpec.feature "User sign up flow", type: :feature do
 
             click_button "Confirm"
 
-            expect(page).to have_content("No apartment Admin has been notified")
-            # expect(page).to have_content("Confirm your apartment address and join!")
+            expect(current_path).to eq(root_path)
+            expect(page).to have_content("Apartment not found. We have submitted your address for verification.")
+
+        end
+
+        scenario "Admin approves pending verification and user can access their account" do
+            # Initial user signup with unverified address
+            visit new_user_registration_path
+            fill_in "Email", with: "pending@test.com"
+            fill_in "Password", with: "password"
+            fill_in "Password confirmation", with: "password"
+            click_button "Sign up"
+    
+            fill_in "First name", with: "pending"
+            fill_in "Last name", with: "user"
+            fill_in "Street address", with: "424 Lincoln Ave"
+            fill_in "Unit number", with: "1363"
+            fill_in "City", with: "Steamboat Springs"
+            fill_in "State", with: "CO"
+            fill_in "Zip code", with: "80487"
+            fill_in "Country", with: "USA"
+            click_button "Next"
+    
+            fill_in "Phone number", with: "(123)123-1234"
+            click_button "Next"
+            click_button "Confirm"
+    
+            expect(page).to have_content("Apartment not found. We have submitted your address for verification.")
+            click_button "Logout"
+
+            # Create and approve the apartment through admin
+            admin = create(:admin_user)
+            apartment = create(:apartment, :steamboat)
+    
+            # User tries to log in after apartment is approved
+            visit new_user_session_path
+            fill_in "Email", with: "pending@test.com"
+            fill_in "Password", with: "password"
+            click_button "Log in"
+
+            click_button "Next"
+
+            expect(page).to have_content("Phone Number")
+            fill_in "Phone number", with: "(123)123-1234"
+
+            click_button "Next"
+
+            expect(page).to have_content("test")
+            expect(page).to have_content("user")
+
+            click_button "Confirm"
+
+            expect(page).to have_content("424 Lincoln Avenue")
             click_button "Join"
 
-            # expect(current_path).to eq(user_dashboard_path)
         end
 
 end
-    
