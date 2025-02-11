91b17e48f12a84961ebaa9a02b5e971f18e50f7a
Jack Gould <Johnhilsmangould@gmail.com>
2025-02-11 14:16:36 -0700

    fix spec

--- spec/features/auth/sign_in_sign_out_spec.rb
+++ spec/features/auth/sign_in_sign_out_spec.rb
@@ -4,6 +4,7 @@ require 'rails_helper'
 RSpec.feature "Sign in and sign out", type: :feature do
     scenario "User signs in and signs out" do
         user = User.create!(email: "test@example.com", password: "password")
+        user.update(onboarding_complete: true)
 
             visit new_user_session_path
 
@@ -12,7 +13,7 @@ RSpec.feature "Sign in and sign out", type: :feature do
             click_button "Log in"
 
             expect(page).to have_content("Signed in successfully")
-
+            save_and_open_page
             click_button "Logout"
 
             expect(page).to have_content("Signed out successfully")
