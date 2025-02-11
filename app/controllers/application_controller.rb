58c707a5981db4759070e9fd67e2a7a45156d704
Jack Gould <Johnhilsmangould@gmail.com>
2025-02-11 13:27:48 -0700

    update admin signin logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,14 @@
 class ApplicationController < ActionController::Base
 
-    def after_sign_in_path_for(resource)
-        return admin_dashboard_path if current_admin_user
-        
-        if resource.onboarding_complete?
-            root_path
-            #will need to upadte onboarding_complete as
-            #that is not method will not work right now
-            # dashboard_path
-        else
-            onboarding_path("personal_info") # Start the onboarding process
-        end
+  def after_sign_in_path_for(resource)
+    if resource.is_a?(AdminUser)
+      admin_dashboard_path
+    else
+      if resource.onboarding_complete?
+        root_path
+      else
+        onboarding_path("personal_info") # Start the onboarding process
+      end
     end
+  end
 end
