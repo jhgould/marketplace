9d25cf62d9ff3f5135e2eda9ca590ca4f05f76d3
Jack Gould <Johnhilsmangould@gmail.com>
2025-02-11 13:30:54 -0700

    add factories

---
+++ spec/factories/admin_pending_apartment_verifications.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :admin_pending_apartment_verification do
+    street_address { "MyString" }
+    city { "MyString" }
+    state { "MyString" }
+    zip_code { "MyString" }
+    country { "MyString" }
+    status { "MyString" }
+  end
+end
