57d590e12768b3c3ba1b571316d34ac2c9f21bd2
Jack Gould <Johnhilsmangould@gmail.com>
2025-02-13 14:52:09 -0700

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,11 @@ Rails.application.routes.draw do
 
   namespace :user do
     resource :dashboard, only: [:show], controller: 'dashboard'
-  end 
+  end
+
+  resources :listings, only: [:new, :create, :show, :index] do
+    resources :steps, only: [:show, :update], controller: 'listings/steps'
+  end
 
   get "up" => "rails/health#show", as: :rails_health_check
 
